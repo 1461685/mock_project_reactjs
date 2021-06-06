a70d9c4a64c09b66e9d81c7d439064423605b8a1
1461685 <nguyettram10@gmail.com>
2021-06-06 13:42:23 +0700

    show list item on UI

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 import './TodoItem.css';
 
-const TodoItem = () => {
+const TodoItem = (props) => {
     return (
         <div className="todo-item">
-            <input type="checkbox"/>
+            <input type="checkbox" checked={props.completed}/>
             <li>
-                <div className="todo-item__title">Todo item number 1</div>
+                <div className="todo-item__title">{props.title}</div>
             </li>
             <div>
                 <button className="todo-item__btnEdit" type="button" name="btn-editTodo">Edit</button>
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,12 +4,20 @@ import TodoItem from './TodoItem';
 import './TodoList.css';
 
 const TodoList = () => {
+    const items = [
+        {id: 'i1', title: 'Todo 1', completed: false},
+        {id: 'i2', title: 'Todo 2', completed: true},
+        {id: 'i3', title: 'Todo 3', completed: false},
+        {id: 'i4', title: 'Todo 4', completed: true}
+    ];
+
     return (
         <div className="todo-list">
             <ul>
-                <TodoItem />
-                <TodoItem />
-                <TodoItem />
+                <TodoItem title={items[0].title} completed={items[0].completed}/>
+                <TodoItem title={items[1].title} completed={items[1].completed}/>
+                <TodoItem title={items[2].title} completed={items[2].completed}/>
+                <TodoItem title={items[3].title} completed={items[3].completed}/>
             </ul>
         </div>
     );
