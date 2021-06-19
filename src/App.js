267156ddd978727305c098bddf6c5b77df21d1fa
Anh Tuan <tuanna2704@gmail.com>
2021-06-19 12:54:38 +0700

    add react router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 import { makeStyles } from '@material-ui/core';
 import TodoHeader from './components/TodoHeader';
@@ -15,10 +16,29 @@ const App = () => {
 	const classes = useStyle();
 
 	return (
-		<div className={classes.root}>
-			<TodoHeader />
-			<TodoList />
-		</div>
+		<>
+			<Router>
+				<Switch>
+					<Route path='/about'>
+						<div>
+							About Page, Click <Link to='/users'>Here</Link> To Go To Users
+							Page
+						</div>
+					</Route>
+					<Route path='/users'>
+						<div>
+							About Page, Click <Link to='/'>Here</Link> To Go To Index Page
+						</div>
+					</Route>
+					<Route path='/'>
+						<div className={classes.root}>
+							<TodoHeader />
+							<TodoList />
+						</div>
+					</Route>
+				</Switch>
+			</Router>
+		</>
 	);
 };
 
