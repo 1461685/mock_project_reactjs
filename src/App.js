41cb04962a0db8304cd6d5d5c8afa6535c5e4d6b
Anh Tuan <tuanna2704@gmail.com>
2021-06-19 13:55:03 +0700

    apply redux

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import store from './app/store';
+import { Provider } from 'react-redux';
 import './App.css';
 import { makeStyles } from '@material-ui/core';
 import TodoHeader from './components/TodoHeader';
 import TodoList from './components/TodoList';
+import TestRedux from './components/TestRedux';
 
 const useStyle = makeStyles({
 	root: {
@@ -14,30 +17,34 @@ const useStyle = makeStyles({
 
 const App = () => {
 	const classes = useStyle();
-
 	return (
 		<>
-			<Router>
-				<Switch>
-					<Route path='/about'>
-						<div>
-							About Page, Click <Link to='/users'>Here</Link> To Go To Users
-							Page
-						</div>
-					</Route>
-					<Route path='/users'>
-						<div>
-							About Page, Click <Link to='/'>Here</Link> To Go To Index Page
-						</div>
-					</Route>
-					<Route path='/'>
-						<div className={classes.root}>
-							<TodoHeader />
-							<TodoList />
-						</div>
-					</Route>
-				</Switch>
-			</Router>
+			<Provider store={store}>
+				<Router>
+					<Switch>
+						<Route path='/about'>
+							<div>
+								About Page, Click <Link to='/users'>Here</Link> To Go To Users
+								Page
+							</div>
+						</Route>
+						<Route path='/test-redux'>
+							<TestRedux></TestRedux>
+						</Route>
+						<Route path='/users'>
+							<div>
+								About Page, Click <Link to='/'>Here</Link> To Go To Index Page
+							</div>
+						</Route>
+						<Route path='/'>
+							<div className={classes.root}>
+								<TodoHeader />
+								<TodoList />
+							</div>
+						</Route>
+					</Switch>
+				</Router>
+			</Provider>
 		</>
 	);
 };
--- src/components/TodoHeader.js
+++ src/components/TodoHeader.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Typography, makeStyles } from '@material-ui/core';
-
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import './TodoHeader.css';
 
 const useStyle = makeStyles({
@@ -14,9 +15,15 @@ const useStyle = makeStyles({
 
 const TodoHeader = () => {
 	const classes = useStyle();
+	const count = useSelector((state) => state.counter.value);
+	console.log(count);
 	return (
 		<div className={classes.root}>
 			<Typography variant='h3'>React Todo App</Typography>
+			<Typography variant='caption'>
+				Value of counter from redux: {count} Back to
+				<Link to='test-redux'> test-redux Page</Link> to update
+			</Typography>
 		</div>
 	);
 };
